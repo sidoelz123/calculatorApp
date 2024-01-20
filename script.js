8fc4cd6d6c05973422a4f8f9d3ccb70a215ba32b
IhzaMZ <sidoelz.123@gmail.com>
2024-01-21 01:10:33 +0700

    fix all function

--- script.js
+++ script.js
@@ -6,10 +6,22 @@ const numbers = document.querySelectorAll(".number");
 const calculatorScreen = document.querySelector('.calculator-screen');
 const operators = document.querySelectorAll(".operator");
 const equalSign = document.querySelector('.equal');
+const persentSign = document.querySelector('.persent')
+const negativeSign = document.querySelector('.negative')
 const allClear = document.querySelector('.all-clear');
-const clearBtn = document.querySelector(".clear");
+const deleteBtn = document.querySelector(".clear");
 const decimal = document.querySelector('.decimal');
 const prevScreen = document.querySelector('.prev-screen')
+
+
+const updateScreen = (number) => {
+    calculatorScreen.value = number;
+}
+const updatePrevScreen = (number) => {
+    prevScreen.value = number;
+}
+updateScreen(currentNumber);
+updatePrevScreen(currentNumber);
 
 const clearAll = () => {
     prevNumber = '';
@@ -17,10 +29,15 @@ const clearAll = () => {
     currentNumber = '0';
 }
 
-allClear.addEventListener('click', () => {
-    clearAll();
-    updateScreen(currentNumber);
-});
+const deleteNumber = ()=>{
+    if (currentNumber === '0'){
+        return
+    }else if (currentNumber.length >= 2){
+        currentNumber = currentNumber.substring(0,currentNumber.length-1);
+    }else{
+        currentNumber = 0
+    }
+}
 
 const inputNumber = (number) => {
     if (currentNumber === '0') {
@@ -30,7 +47,7 @@ const inputNumber = (number) => {
     }
 }
 
-inputDecimal = (dot) => {
+const inputDecimal = (dot) => {
     if (currentNumber.includes('.')) {
         return;
     }
@@ -45,32 +62,12 @@ const inputOperator = (operator) => {
     currentNumber = '0';
 }
 
-const updateScreen = (number) => {
-    calculatorScreen.value = number;
+const inverseNumber = ()=>{
+    if (currentNumber === '0'){
+        return
+    }
+    currentNumber = currentNumber * -1;
 }
-const updatePrevScreen = (number) => {
-    prevScreen.value = number;
-}
-
-decimal.addEventListener('click', (event) => {
-    inputDecimal(event.target.value);
-    updateScreen(currentNumber);
-});
-
-
-operators.forEach((operator) => {
-    operator.addEventListener("click", (event) => {
-        inputOperator(event.target.value);
-    });
-});
-
-numbers.forEach((number) => {
-    number.addEventListener("click", (event) => {        
-        inputNumber(event.target.value);
-        updateScreen(currentNumber);
-        
-    });
-});
 
 const calculate = () => {
     let result = '';
@@ -81,7 +78,7 @@ const calculate = () => {
         case "-":
             result = parseFloat(prevNumber) - parseFloat(currentNumber);
             break;
-        case "*":
+        case "×":
             result = parseFloat(prevNumber) * parseFloat(currentNumber);
             break;
         case "/":
@@ -95,8 +92,54 @@ const calculate = () => {
     
 }
 
-equalSign.addEventListener('click', () => {
-    console.log(calculationOperator);
-    calculate();
+deleteBtn.addEventListener('click', () => {
+    deleteNumber()
+    updateScreen(currentNumber)
+    console.log(currentNumber);
+});
+
+allClear.addEventListener('click', () => {
+    clearAll();
+    updateScreen(currentNumber);
+    updatePrevScreen(currentNumber);
+});
+
+
+decimal.addEventListener('click', (event) => {
+    inputDecimal(event.target.value);
     updateScreen(currentNumber);
 });
+
+
+operators.forEach((operator) => {
+    operator.addEventListener("click", (event) => {
+        inputOperator(event.target.value);
+        updatePrevScreen(`${prevNumber} ${event.target.value}`);
+        updateScreen(0)
+    });
+});
+
+numbers.forEach((number) => {
+    number.addEventListener("click", (event) => {        
+        inputNumber(event.target.value);
+        updateScreen(currentNumber);
+    });
+});
+
+
+equalSign.addEventListener('click', () => {
+    updatePrevScreen(`${prevNumber} ${calculationOperator} ${currentNumber}`);
+    calculate();
+    updateScreen(currentNumber);
+    currentNumber = currentNumber;
+});
+persentSign.addEventListener('click',()=>{
+    currentNumber = currentNumber / 100
+    updateScreen(currentNumber)
+    console.log(currentNumber);
+})
+negativeSign.addEventListener('click',()=>{
+   inverseNumber()
+   updateScreen(currentNumber)
+   console.log(currentNumber);
+})
